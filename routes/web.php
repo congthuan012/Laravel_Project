392aa8f8be0c021523b7a0837ea467622844f03b
hcthuan <thuanhuynh@pixelcent.com>
2021-05-11 14:07:19 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,6 @@ Route::get('/cache/all', function () {
     Artisan::call('key:generate');
     Artisan::call('config:cache');
     Artisan::call('view:cache');
-    Artisan::call('route:cache');
 
 });
 
